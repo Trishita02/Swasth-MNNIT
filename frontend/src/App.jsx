1902062565baf3640ee334b789e5e09c3027d913
Trishita <trishu.kesarwani06@gmail.com>
2025-04-04 01:26:03 +0530

    UI for adding,removing,manipulating admin,staff,doctor details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Admin from './pages/admin/adminDashboard.jsx';
 import ChangePasswordPage from "./pages/ChangePassword.jsx";
+import ManageUsers from "./pages/admin/ManageUsers.jsx"
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Home />} />
         <Route path="/admin/dashboard"  element={<Admin/>}/>
         <Route path="/admin/change-password" element={<ChangePasswordPage/>}/>
+        <Route path="/admin/manage-users" element={<ManageUsers/>}/>
       </Routes>
     </BrowserRouter>
   );
